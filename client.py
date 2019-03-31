f5e92f0c4c6a4ce42a1e42122d36ad050999246e
Amitrajit Bose <amitrajitbose9@gmail.com>
2019-03-31 10:36:27 +0530

    Fixed the abrupt error when client exits chat window

--- client.py
+++ client.py
@@ -1,15 +1,22 @@
-# chat_client.py
+# client.py
 import sys
 import socket
 import select
  
 def chat_client():
     if(len(sys.argv) < 3) :
-        print( 'Usage : python3 chat_client.py hostname port')
-        sys.exit()
+        print( 'Usage : python3 client.py [hostname] [port]')
+        print( 'Default : [hostname] = localhost, [port] = 7500')
 
-    host = sys.argv[1]
-    port = int(sys.argv[2])
+    try:
+        host = sys.argv[1]
+    except:
+        host = 'localhost'
+
+    try:
+        port = int(sys.argv[2])
+    except:
+        port = 7500
     
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(10)
@@ -21,7 +28,7 @@ def chat_client():
         print ('Unable to connect')
         sys.exit()
      
-    print ('Connected to remote host. You can start sending messages')
+    print ('Connected to remote host. You can start sending messages. Ctrl+C to Exit.')
     username = input("Enter Your Username: ")
     sys.stdout.write('<%s> ' %username); sys.stdout.flush()
     s.send(username.encode()) 
@@ -30,8 +37,11 @@ def chat_client():
         socket_list = [sys.stdin, s]
          
         # Get the list sockets which are readable
-        read_sockets,write_sockets,error_sockets = select.select(socket_list , [], [])
-         
+        try:
+            read_sockets,write_sockets,error_sockets = select.select(socket_list , [], [])
+        except:
+            sys.exit("\n")
+
         for sock in read_sockets:            
             if sock == s:
                 # incoming message from remote server, s
