24cebf88e74c8bd0e044860fc7dba8999c5d59d7
Amitrajit Bose <amitrajitbose9@gmail.com>
2019-03-31 08:51:38 +0530

    Updated script to support Python3, and alias names of chat members

--- client.py
+++ client.py
@@ -1,59 +1,55 @@
-# client.py
-
-import sys, socket, select
-
-def welcome_message():
-    print ''
-    print "\n\n\t\t\t\tWELCOME TO CHATTY v1.0\t\t\n\t\t\t\t----------------------"
-    print "Chatty is an Open Source Python Command Line Chat App Built On Python Web Sockets"
-    print 'Author: Amitrajit Bose & Riddhi Nahata at University of Engineering & Management, Kolkata'
-    print 'This is a Minor Academic Project and Must Not Be Used For Emergency Communication Purpose'
-    print '\n'
-
+# chat_client.py
+import sys
+import socket
+import select
+ 
 def chat_client():
     if(len(sys.argv) < 3) :
-        print 'Usage : python chat_client.py HOSTNAME PORT'
+        print( 'Usage : python3 chat_client.py hostname port')
         sys.exit()
 
     host = sys.argv[1]
     port = int(sys.argv[2])
-     
+    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
+    s.settimeout(10)
      
     # connect to remote host
     try :
         s.connect((host, port))
     except :
-        print 'Unable to connect'
+        print ('Unable to connect')
         sys.exit()
-    
-    welcome_message()
-    print 'Connected to remote host. You can start sending messages'
-    sys.stdout.write('[Me] '); sys.stdout.flush()
      
+    print ('Connected to remote host. You can start sending messages')
+    username = input("Enter Your Username: ")
+    sys.stdout.write('<%s> ' %username); sys.stdout.flush()
+    s.send(username.encode()) 
+    
     while 1:
         socket_list = [sys.stdin, s]
          
         # Get the list sockets which are readable
-        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+        read_sockets,write_sockets,error_sockets = select.select(socket_list , [], [])
          
         for sock in read_sockets:            
             if sock == s:
                 # incoming message from remote server, s
-                data = sock.recv(4096)
+                data = (sock.recv(4096))
+                data = data.decode()
                 if not data :
-                    print '\nDisconnected from chat server'
+                    print('\nDisconnected from chat server')
                     sys.exit()
                 else :
-                    #print data
+                    #print (data)
                     sys.stdout.write(data)
-                    sys.stdout.write('[Me] '); sys.stdout.flush()
+                    sys.stdout.write('<%s> ' %username); sys.stdout.flush()     
+            
             else :
                 # user entered a message
-                msg = sys.stdin.readline()
+                msg = sys.stdin.readline().encode()
                 s.send(msg)
-                sys.stdout.write('[Me] '); sys.stdout.flush() 
+                sys.stdout.write('<%s> ' %username); sys.stdout.flush() 
 
 if __name__ == "__main__":
     sys.exit(chat_client())
--- server.py
+++ server.py
@@ -1,36 +1,39 @@
-# server.py
+# chat_server.py
  
 import sys, socket, select
 
-HOST = '' 
+HOST = '' #nothing indicates localhost
 SOCKET_LIST = []
 RECV_BUFFER = 4096 
-PORT = 9009
+PORT = 7500
 
 def chat_server():
-
+    
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))
     server_socket.listen(10)
+
+    username = dict()
  
     # add server socket object to the list of readable connections
     SOCKET_LIST.append(server_socket)
  
-    print "Chat server started on port " + str(PORT)
+    print ("Chat server started on port " + str(PORT))
  
     while 1:
 
         # get the list sockets which are ready to be read through select
         # 4th arg, time_out  = 0 : poll and never block
-        ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST,[],[],0)
+        read_sockets,write_sockets,error_sockets = select.select(SOCKET_LIST,[],[],0)
       
-        for sock in ready_to_read:
+        for sock in read_sockets:
             # a new connection request recieved
             if sock == server_socket: 
                 sockfd, addr = server_socket.accept()
                 SOCKET_LIST.append(sockfd)
-                print "Client (%s, %s) connected" % addr
+                #print (sockfd, addr)
+                print ("Client (%s, %s) connected" % addr)
                  
                 broadcast(server_socket, sockfd, "[%s:%s] entered our chatting room\n" % addr)
              
@@ -39,10 +42,16 @@ def chat_server():
                 # process data recieved from client, 
                 try:
                     # receiving data from the socket.
-                    data = sock.recv(RECV_BUFFER)
+                    data = sock.recv(RECV_BUFFER).decode()
+                    #print(sock.getpeername(), data)
+
+                    if(sock.getpeername() not in username):
+                        username[sock.getpeername()] = data
+                        continue
+
                     if data:
                         # there is something in the socket
-                        broadcast(server_socket, sock, "\r" + '[' + str(sock.getpeername()) + '] ' + data)  
+                        broadcast(server_socket, sock, "\r" + '<' + username[sock.getpeername()] + '> ' + data)  
                     else:
                         # remove the socket that's broken    
                         if sock in SOCKET_LIST:
@@ -64,7 +73,7 @@ def broadcast (server_socket, sock, message):
         # send the message only to peer
         if socket != server_socket and socket != sock :
             try :
-                socket.send(message)
+                socket.send(message.encode())
             except :
                 # broken socket connection
                 socket.close()
@@ -74,3 +83,6 @@ def broadcast (server_socket, sock, message):
  
 if __name__ == "__main__":
     sys.exit(chat_server())
+
+
+         
